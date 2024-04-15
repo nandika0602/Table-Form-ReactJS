9eb475b17fcbef97c3212cf4380055ba999f65e1
nandika0602 <nandika.d0602@gmail.com>
2024-04-15 15:11:50 +0530

    update

---
+++ src/TableForm.js
@@ -0,0 +1,269 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+//instead of id (able to use id, index, uuid generator)
+//instead of || (ternary, useState return stmt)
+//amount 4 col should have sum of amt
+//all rows amt should display at bottom
+//delete only selected rows(checkbox)
+//select all items and vice versa
+//while selecting all items, if any of the rows is unchecked at that time, the select all checkbox also have to uncheck
+
+const TableForm = () => {
+  const tableHeader = [
+    "FirstName",
+    "MiddleName",
+    "PhoneNo",
+    "Marital Status",
+    "Amount 1",
+    "Amount 2",
+    "Amount 3",
+    "Amount 4",
+    "check",
+    "Action",
+  ];
+  const obj = {
+    id: "",
+    firstName: "",
+    middleName: "",
+    phoneNo: "",
+    maritalStatus: "",
+    Amount1: 0,
+    Amount2: 0,
+    Amount3: 0,
+    Amount4: 0,
+    isChecked: false,
+  };
+  const [totalAmt, setTotalAmt] = useState(() => {
+    const amount = localStorage.getItem("total");
+    return amount ? JSON.parse(amount) : 0;
+  });
+  const [tableData, setTableData] = useState(() => {
+    const storedData = localStorage.getItem("data");
+    return storedData ? JSON.parse(storedData) : [];
+  });
+  const [isChecked, setIsChecked] = useState(false);
+
+  const add = () => {
+    setTableData([...tableData, { ...obj, id: new Date().getTime() }]);
+  };
+
+  const deleteRow = (id) => {
+    const deletedTable = tableData.filter((t) => t.id !== id);
+    setTableData(deletedTable);
+  };
+
+  const handleEvent = (e, i) => {
+    setTableData((prevData) => {
+      const newList = [...prevData];
+      const rowData = newList[i];
+      if (e.target.name === "isChecked") {
+        rowData[e.target.name] = e.target.checked;
+        setIsChecked(false);
+      } else {
+        rowData[e.target.name] = e.target.value;
+      }
+      if (e.target.name === "Amount1" || "Amount2" || "Amount3") {
+        rowData["Amount4"] =
+          Number(rowData.Amount1) + +rowData.Amount2 + +rowData.Amount3;
+      }
+      newList[i] = rowData;
+      console.log(newList[i]);
+      return newList;
+    });
+  };
+
+  const saveData = () => {
+    setTableData([...tableData]);
+    const amount = tableData.reduce((acc, ini) => {
+      return ini.Amount4 + acc;
+    }, 0);
+    setTotalAmt(amount);
+    localStorage.setItem("total", JSON.stringify(amount));
+    localStorage.setItem("data", JSON.stringify(tableData));
+    alert("successfully saved");
+  };
+
+  const deleteAll = (tableData) => {
+    const list = tableData.filter((li) => !li.isChecked);
+    if (window.confirm("Do you wanna delete all the users data?")) {
+      setTableData(list);
+      setIsChecked(false);
+      localStorage.setItem("data", JSON.stringify([]));
+    }
+  };
+
+  const selectAll = (e) => {
+    setIsChecked(true);
+    if (e.target.checked) {
+      //in map assigning will mutate the original array as well
+      tableData.map((li) => {
+        return (li.isChecked = true);
+      });
+    } else {
+      setIsChecked(false);
+      tableData.map((li) => {
+        return (li.isChecked = false);
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div className="table">
+        <table>
+          {tableHeader.map((header, index) => (
+            <th key={index} style={{ width: "0%" }}>
+              {header === "check" ? (
+                <input
+                  type="checkbox"
+                  className="check-box1"
+                  value={"selectAll"}
+                  onChange={(e) => selectAll(e)}
+                  checked={isChecked}
+                  name="checked"
+                ></input>
+              ) : (
+                header
+              )}
+            </th>
+          ))}
+          <tbody className="body">
+            {tableData.map((t, i) => (
+              <tr key={t.id}>
+                <td>
+                  <input
+                    name="firstName"
+                    value={t.firstName}
+                    type="text"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="middleName"
+                    value={t.middleName}
+                    type="text"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="phoneNo"
+                    value={t.phoneNo}
+                    type="number"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  />
+                </td>
+                <td className="in">
+                  <select
+                    name="maritalStatus"
+                    value={t.maritalStatus}
+                    className="input1"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  >
+                    <option value="" className="inputOpt">
+                      Select marital status
+                    </option>
+                    <option value="Married" className="inputOpt">
+                      Married
+                    </option>
+                    <option value="Unmarried" className="inputOpt">
+                      Unmarried
+                    </option>
+                  </select>
+                </td>
+                <td>
+                  <input
+                    name="Amount1"
+                    value={t.Amount1}
+                    type="number"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="Amount2"
+                    value={t.Amount2}
+                    type="number"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="Amount3"
+                    value={t.Amount3}
+                    type="number"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                  />
+                </td>
+                <td>
+                  <input
+                    name="Amount4"
+                    value={t.Amount4}
+                    type="number"
+                    className="input"
+                    onChange={(e) => handleEvent(e, i)}
+                    id={t.id}
+                    disabled
+                  />
+                </td>
+                <td>
+                  <input
+                    type="checkbox"
+                    className="check-box"
+                    name="isChecked"
+                    value={`Delete${t.id}`}
+                    checked={t.isChecked}
+                    onChange={(e) => handleEvent(e, i)}
+                  />
+                </td>
+                <td>
+                  <FontAwesomeIcon
+                    onClick={() => deleteRow(t.id)}
+                    icon={faTrash}
+                    style={{
+                      color: "#FF0000",
+                      marginLeft: "20px",
+                      cursor: "pointer",
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="footer">
+        <h4 style={{ float: "right", marginRight: "150px", marginTop: "10px" }}>
+          TOTAL AMOUNT: {totalAmt}
+        </h4>
+        <button onClick={add} className="btn-add">
+          ADD
+        </button>
+        <button onClick={() => saveData(tableData)} className="btn-save">
+          SAVE
+        </button>
+        <button onClick={() => deleteAll(tableData)} className="btn-del">
+          DELETE
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TableForm;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +13,66 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// const handleEvent = (e, i) => {
+//   // const a = tableData.map((t) => {
+//   //   if (e.target.id == t.id) {
+//   //     return { ...t, [e.target.name]: e.target.value };
+//   //   }
+//   //   return t;
+//   // });
+//   // setTableData(a);
+//   // if (e.target.checked) {
+//   //   // console.log(e.target.name, e.target.value, e.target.id);
+//   //   // setCheckedList([...checkedList, e.target.value])
+//   //   setCheckedList((preData) => {
+//   //     // console.log(preData, "preeee");
+//   //     const newArr = [...preData];
+//   //     // newArr.push(e.target.value)
+//   //     const newArr1 = [...newArr, e.target.value];
+//   //     // console.log(newArr1, "neww");
+//   //     return newArr1;
+//   //   });
+//   // } else {
+//   //   setIsChecked(true);
+//   //   const unChecked = checkedList.filter((li) => {
+//   //     console.log(li, e.target.value);
+//   //     return li !== e.target.value;
+//   //   });
+//   //   setCheckedList(unChecked);
+//   // }
+//   // if(e.target.checked)
+//   // console.log(checkedList, "checkedList");
+//   setTableData((prevData) => {
+//     const newList = [...prevData];
+//     const rowData = newList[i];
+//     // console.log(rowData, "change", "rowData");
+//     if (e.target.name === "isChecked") {
+//       console.log(e.target.checked, e.target.value, e.target.name);
+//       rowData[e.target.name] = e.target.checked;
+//       // obj.isChecked = false
+//       setIsChecked(true);
+//     } else {
+//       console.log(rowData);
+//       rowData[e.target.name] = e.target.value;
+//       // const a = tableHeader.map((h,i) => {
+//       //   if(h === 'check') {
+//       //   }
+//       // })
+//       rowData["checked"] = false;
+//     }
+//     if (e.target.name === "Amount1" || "Amount2" || "Amount3") {
+//       rowData["Amount4"] =
+//         Number(rowData.Amount1) + +rowData.Amount2 + +rowData.Amount3;
+//     }
+//     newList[i] = rowData;
+//     return newList;
+//   });
+
+//   // const s = [...tableData, { ...obj }];
+//   // console.log(s, "s ", tableData);
+//   // const q = [...tableData, { ...obj, [e.target.name]: e.target.value }];
+//   // console.log(q, "q");
+//   // setTableData([...tableData, { ...obj, [e.target.name]: e.target.value }]);
+// };
